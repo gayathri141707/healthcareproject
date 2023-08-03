17c6890ff40db3939e181e3833f8c2c6f8c9dba7
gayathri <gayathri141707@gmail.com>
2023-08-03 14:52:01 +0530

    health care project files first commit

---
+++ src/main/java/com/project/staragile/MedicureApplication.java
@@ -0,0 +1,14 @@
+package com.project.staragile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class MedicureApplication extends SpringBootServletInitializer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicureApplication.class, args);
+	}
+
+}
